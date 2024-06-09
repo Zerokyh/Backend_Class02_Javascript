c4bd727e01e35f63b0e3b21c813e1122e4bdb5c2
Zerokyh <109337472+Zerokyh@users.noreply.github.com>
2024-06-09 21:26:50 +0900

    Delete day08 directory

--- day08/data/index.js
+++
@@ -1,3 +0,0 @@
-const foods = document.querySelectorAll(".food"); // querySelector는 동일한 클래스명이 있으면 제일 처음꺼, querySelectorAll은 동일 클래스명 전부다 가져옴
-foods.forEach((v) => console.log(v.dataset.food));
-foods.forEach((v) => console.log(v.dataset.price));
